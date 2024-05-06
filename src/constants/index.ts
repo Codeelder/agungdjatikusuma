0c57a09668bcf98f26a6fee703296f2b1149ad70
Kaizen <167880851+Codeelder@users.noreply.github.com>
2024-05-06 15:53:25 +0700

    feat(data): fix data dummy

---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+export const CARDL = [
+    {
+        id: 1,
+        title: "Pengembangan perangkat lunak",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam porro odio dolorum saepe debitis nisi",
+        image: "/layanan/toolkit-1.png",
+    },
+    {
+        id: 2,
+        title: "Perdagangan",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam porro odio dolorum saepe debitis nisi",
+        image: "/layanan/toolkit-4.png"
+    }
+]
+
+export const CARDS = [
+    {
+        id: 1,
+        title: "Reparasi",
+        image: "/layanan/toolkit-2.png"
+    },
+    {
+        id: 2,
+        title: "Ekspedisi",
+        image: "/layanan/toolkit-3.png"
+    }
+]
---
+++ src/types/layanan.ts
@@ -0,0 +1,12 @@
+export type CardL = {
+    id: number
+    title: string
+    desc: string
+    image: string
+}
+
+export type CardS = {
+    id: number
+    title: string
+    image: string
+}
