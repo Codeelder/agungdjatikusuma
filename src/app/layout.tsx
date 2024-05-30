eb4c3c4e9367edaa4206f4351201f3d388cd7610
Kaizen <119792622+KaizenSheesh@users.noreply.github.com>
2024-05-31 00:34:16 +0700

    fix(layout): fix image metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,9 @@
-import type { Metadata } from "next"
-
 import { Footer } from "@/components/molecules/footer"
 import { Navbar } from "@/components/molecules/navbar"
 
-import "./globals.css"
+import { metadata } from "../lib/metada"
 
-export const metadata: Metadata = {
-  title: {
-    template: "Agung Djati Kusuma | %s",
-    default: "Agung Djati Kusuma",
-  },
-  icons: "/logo.png",
-  openGraph: {
-    title: "Agung Djati Kusuma",
-    description: "Layanan ekspedisi, perangkat lunak, dan perdagangan eceran berkualitas tinggi.",
-    url: process.env.NEXT_PUBLIC_APP_URL,
-    siteName: "Agung Djati Kusuma",
-    images: [
-      {
-        url: `${process.env.NEXT_PUBLIC_APP_URL}thumbnail.png`,
-        width: 508,
-        height: 508,
-      },
-    ],
-    type: "website",
-  },
-}
+import "./globals.css"
 
 export default function RootLayout({
   children,
@@ -34,6 +12,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title.default}</title>
+        <meta name="description" content={metadata.openGraph.description} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta
+          property="og:image:width"
+          content={metadata.openGraph.images[0].width.toString()}
+        />
+        <meta
+          property="og:image:height"
+          content={metadata.openGraph.images[0].height.toString()}
+        />
+        <meta property="og:type" content={metadata.openGraph.type} />
+      </head>
       <body>
         <Navbar />
         <div className="mb-[100px] mt-[68px] overflow-x-clip">{children}</div>
---
+++ src/lib/metada.js
@@ -0,0 +1,21 @@
+export const metadata = {
+    title: {
+      template: "Agung Djati Kusuma | %s",
+      default: "Agung Djati Kusuma",
+    },
+    icons: "/logo.png",
+    openGraph: {
+      title: "Agung Djati Kusuma",
+      description: "Layanan ekspedisi, perangkat lunak, dan perdagangan eceran berkualitas tinggi.",
+      url: process.env.NEXT_PUBLIC_APP_URL,
+      siteName: "Agung Djati Kusuma",
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_APP_URL}/thumbnail.png`,
+          width: 1920,
+          height: 1080,
+        },
+      ],
+      type: "website",
+    },
+  }
